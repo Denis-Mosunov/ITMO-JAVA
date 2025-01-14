933d110f8fc58060cd02c77cb60a8d751f8648df
Denis-Mosunov <dnsmosunov@gmail.com>
2025-01-14 21:04:33 +0300

    New

---
+++ src/Lab3/Airplane.java
@@ -0,0 +1,14 @@
+package Lab3;
+
+public class Airplane {
+
+    public class Wing {
+        private int weight;
+
+        public Wing(int weight) {this.weight = weight;}
+
+        public void PrintWeight(){
+            System.out.printf("Имеет вес %d кг.", weight);
+        }
+    }
+}
