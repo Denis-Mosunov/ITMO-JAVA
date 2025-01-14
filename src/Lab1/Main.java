f42ac104e539b814db17383d1e0f049b669b39f3
Denis-Mosunov <dnsmosunov@gmail.com>
2025-01-14 22:16:09 +0300

    1 Path

--- src/Lab1/Main.java
+++ src/Lab1/Main.java
@@ -3,31 +3,7 @@ package Lab1;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Exercise1();
-        Exercise2();
-        Exercise3();
-        Exercise4();
-        Exercise5();
-        Exercise6();
-    }
-    private static void Exercise1() {
-    }
 
-    private static void Exercise2() {
-    }
-
-    private static void Exercise3() {
-    }
-
-    private static void Exercise4() {
-    }
-
-    private static void Exercise5() {
-    }
-
-    private static void Exercise6() {
-    }
 }
 
 
